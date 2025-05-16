dbe5677aee463f918114032f2073c0f43f0a4616
HanifRidal <hanifridal17@gmail.com>
2025-05-16 15:12:34 +0700

    bulid component (form, breadcrumb) & litle page detail

--- src/utils/formatNumber.js
+++ src/utils/formatNumber.js
@@ -1,4 +1,11 @@
-export default (number) => {
+// export default (number) => {
+//   const formatNumbering = new Intl.NumberFormat("id-ID");
+//   return formatNumbering.format(number);
+// };
+
+const formatNumber = (number) => {
   const formatNumbering = new Intl.NumberFormat("id-ID");
   return formatNumbering.format(number);
 };
+
+export default formatNumber;
