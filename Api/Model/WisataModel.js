063c6dfc417dd7bc8fc1eee94c7757c793b457ed
DaffafauzanD <daffafauzand24@gmail.com>
2025-06-11 15:17:25 +0700

    feat: refactor update and remove functions to use transactions and improve error handling

--- Api/Model/WisataModel.js
+++ Api/Model/WisataModel.js
@@ -82,30 +82,44 @@ async function create(tujuanWisata, imageUrls) {
   }
 }
 
+// In your WisataModel.js, update these functions:
+
 async function update(id, tujuanWisata, imageUrls) {
-  // Begin transaction
-  const connection = await executeQuery("BEGIN TRANSACTION", [], [], true);
+  // Create a pool for our transaction
+  const sql = require('mssql');
+  const pool = await sql.connect(require('../db').config);
+  
+  // Start a transaction properly
+  const transaction = new sql.Transaction(pool);
+  await transaction.begin();
   
   try {
     // First, update the ImageUrls table if we have imageId
     if (imageUrls && imageUrls.imageId) {
-      const updateImageQuery = `
+      const updateImageRequest = transaction.request();
+      updateImageRequest.input('url1', sql.NVarChar, imageUrls.url1);
+      updateImageRequest.input('url2', sql.NVarChar, imageUrls.url2);
+      updateImageRequest.input('imageId', sql.Int, imageUrls.imageId);
+      
+      await updateImageRequest.query(`
         UPDATE ImageUrls 
         SET Url_1 = @url1, Url_2 = @url2
         WHERE Id_Image = @imageId;
-      `;
-      
-      await executeQuery(
-        updateImageQuery,
-        [imageUrls.url1, imageUrls.url2, imageUrls.imageId],
-        ["url1", "url2", "imageId"],
-        false,
-        connection
-      );
+      `);
     }
     
     // Then, update the TujuanWisata table
-    const updateTujuanQuery = `
+    const updateTujuanRequest = transaction.request();
+    updateTujuanRequest.input('id', sql.Int, id);
+    updateTujuanRequest.input('nama', sql.NVarChar, tujuanWisata.nama);
+    updateTujuanRequest.input('kota', sql.NVarChar, tujuanWisata.kota);
+    updateTujuanRequest.input('keterangan', sql.NVarChar, tujuanWisata.keterangan || null);
+    updateTujuanRequest.input('type', sql.NVarChar, tujuanWisata.type);
+    updateTujuanRequest.input('imgUrl', sql.NVarChar, tujuanWisata.imgUrl);
+    updateTujuanRequest.input('popularity', sql.Int, tujuanWisata.popularity || 0);
+    
+    // Only include ImageID in update if it exists
+    let updateQuery = `
       UPDATE TujuanWisata 
       SET NamaTempat = @nama, 
           Kota = @kota, 
@@ -113,75 +127,68 @@ async function update(id, tujuanWisata, imageUrls) {
           type = @type, 
           ImgUrl = @imgUrl, 
           Popularity = @popularity
-          ${imageUrls && imageUrls.imageId ? ', ImageID = @imageId' : ''}
-      WHERE TujuanID = @id;
     `;
     
-    const params = [
-      tujuanWisata.nama,
-      tujuanWisata.kota,
-      tujuanWisata.keterangan || null,
-      tujuanWisata.type,
-      tujuanWisata.imgUrl,
-      tujuanWisata.popularity || 0,
-      id
-    ];
-    
-    const paramNames = ["nama", "kota", "keterangan", "type", "imgUrl", "popularity", "id"];
-    
-    // Add imageId to params if we're updating it
     if (imageUrls && imageUrls.imageId) {
-      params.push(imageUrls.imageId);
-      paramNames.push("imageId");
+      updateQuery += `, ImageID = @imageId`;
+      updateTujuanRequest.input('imageId', sql.Int, imageUrls.imageId);
     }
     
-    await executeQuery(
-      updateTujuanQuery,
-      params,
-      paramNames,
-      false,
-      connection
-    );
+    updateQuery += ` WHERE TujuanID = @id`;
     
-    // Commit transaction
-    await executeQuery("COMMIT", [], [], false, connection);
+    await updateTujuanRequest.query(updateQuery);
+    
+    // Commit the transaction
+    await transaction.commit();
     
     return { success: true };
   } catch (error) {
     // Rollback transaction in case of error
-    await executeQuery("ROLLBACK", [], [], false, connection);
+    await transaction.rollback();
     throw error;
+  } finally {
+    pool.close();
   }
 }
 
 async function remove(id) {
+  // Create a pool for our transaction
+  const sql = require('mssql');
+  const pool = await sql.connect(require('../db').config);
+  
   // Get the ImageID associated with this TujuanWisata
-  const getTujuanQuery = "SELECT ImageID FROM TujuanWisata WHERE TujuanID = @id";
-  const tujuanResult = await executeQuery(getTujuanQuery, [id], ["id"], false);
+  const getTujuanRequest = pool.request();
+  getTujuanRequest.input('id', sql.Int, id);
+  const tujuanResult = await getTujuanRequest.query("SELECT ImageID FROM TujuanWisata WHERE TujuanID = @id");
   const imageId = tujuanResult.recordset[0]?.ImageID;
-
-  // Begin transaction
-  const connection = await executeQuery("BEGIN TRANSACTION", [], [], true);
+  
+  // Start a transaction properly
+  const transaction = new sql.Transaction(pool);
+  await transaction.begin();
   
   try {
     // First, delete from TujuanWisata
-    const deleteTujuanQuery = "DELETE FROM TujuanWisata WHERE TujuanID = @id";
-    await executeQuery(deleteTujuanQuery, [id], ["id"], false, connection);
+    const deleteTujuanRequest = transaction.request();
+    deleteTujuanRequest.input('id', sql.Int, id);
+    await deleteTujuanRequest.query("DELETE FROM TujuanWisata WHERE TujuanID = @id");
     
     // Then, delete from ImageUrls if there was an associated image
     if (imageId) {
-      const deleteImageQuery = "DELETE FROM ImageUrls WHERE Id_Image = @imageId";
-      await executeQuery(deleteImageQuery, [imageId], ["imageId"], false, connection);
+      const deleteImageRequest = transaction.request();
+      deleteImageRequest.input('imageId', sql.Int, imageId);
+      await deleteImageRequest.query("DELETE FROM ImageUrls WHERE Id_Image = @imageId");
     }
     
     // Commit transaction
-    await executeQuery("COMMIT", [], [], false, connection);
+    await transaction.commit();
     
     return { success: true };
   } catch (error) {
     // Rollback transaction in case of error
-    await executeQuery("ROLLBACK", [], [], false, connection);
+    await transaction.rollback();
     throw error;
+  } finally {
+    pool.close();
   }
 }
 
